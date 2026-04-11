2a230a83416766237bf14f5895d297ba1b3ed4cd
Mohammad Ishan Ahmad <mdishanahmad@gmail.com>
2026-04-12 03:47:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,114 @@
-//your JS code here. If required.
+let inputContainerEl = document.querySelector("#inputContainer");
+let player1El = document.querySelector("#player-1");
+let player2El = document.querySelector("#player-2");
+let submitButtonEl = document.querySelector("#submit");
+let gameContainerEl = document.querySelector("#game-container");
+let messageEl = document.querySelector("#message");
+let cells = document.querySelectorAll(".cell");
+let boardEl = document.querySelector("#board");
+let player1Name = "";
+let player2Name = "";
+let currentPlayerMove = "X";
+let currentPlayerName;
+let board = [
+  ["", "", ""],
+  ["", "", ""],
+  ["", "", ""],
+];
+submitButtonEl.addEventListener("click", () => {
+  if (player1El.value !== "" && player2El.value !== "") {
+    player1Name = player1El.value.trim();
+    player2Name = player2El.value.trim();
+    currentPlayerName = player1Name;
+    inputContainerEl.classList.add("hide");
+    gameContainerEl.classList.remove("hide");
+    messageEl.textContent = `${currentPlayerName}, you're up`;
+  }
+});
+
+cells.forEach((cell, index) => {
+  cell.addEventListener("click", () => {
+    if (cell.textContent !== "") return;
+    let row = cell.getAttribute("data-row");
+    row = parseInt(row);
+    let col = cell.getAttribute("data-col");
+    col = parseInt(col);
+    board[row][col] = currentPlayerMove;
+    console.log(board);
+    cell.textContent = currentPlayerMove;
+    cell.classList.add("disabled");
+    let winnerFound = checkWinner();
+     let checkForDraw = checkDraw();
+    if (winnerFound) {
+      messageEl.textContent = `${currentPlayerName}, congratulations you won!`;
+       boardEl.classList.add("disabled");
+      return;
+    }
+
+   
+     else if (checkForDraw) {
+     messageEl.textContent = `Draw`;
+       boardEl.classList.add("disabled");
+      return;
+    }
+else
+{
+currentPlayerMove = currentPlayerMove === "X" ? "O" : "X";
+    currentPlayerName =
+      currentPlayerName === player1Name ? player2Name : player1Name;
+    messageEl.textContent = `${currentPlayerName}, you're up`;
+}
+    
+  });
+});
+
+function checkWinner() {
+  for (let i = 0; i < 3; i++) {
+    if (
+      board[i][0] &&
+      board[i][0] === board[i][1] &&
+      board[i][1] === board[i][2]
+    ) {
+      return true;
+    }
+  }
+
+  for (let i = 0; i < 3; i++) {
+    if (
+      board[0][i] &&
+      board[0][i] === board[1][i] &&
+      board[1][i] === board[2][i]
+    ) {
+      return true;
+    }
+  }
+
+  if (
+    board[0][0] &&
+    board[0][0] === board[1][1] &&
+    board[1][1] === board[2][2]
+  ) {
+    return true;
+  }
+
+  if (
+    board[0][2] &&
+    board[0][2] === board[1][1] &&
+    board[1][1] === board[2][0]
+  ) {
+    return true;
+  }
+
+  return false;
+}
+
+function checkDraw() {
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (board[i][j] === "") {
+        return false;
+      }
+    }
+  }
+  return true;
+}
